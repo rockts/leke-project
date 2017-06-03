fee7c18367bae6804b213c3628e207019ddd4e45
gaopeng <gaopeng@lekee.cc>
2017-06-04 01:53:14 +0800

    《webpack：模块打包》完成

---
+++ name.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from "react";
+
+class Name extends React.Component {
+  render() {
+    return (
+      <div>
+        ddddd dddd~ lekee.cc <input />
+      </div>
+    );
+  }
+}
+
+export { Name as default };
